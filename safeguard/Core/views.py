00d966d3c92ee546f9c4c9d375fef771a868d6a6
lolly1259998 <hadilbouhachem12@gmail.com>
2025-10-15 14:19:35 +0100

    add user in views

--- safeguard/Core/views.py
+++ safeguard/Core/views.py
@@ -14,3 +14,14 @@ class ControlCenterViewSet(viewsets.ModelViewSet):
     queryset = ControlCenter.objects.all()
     serializer_class = ControlCenterSerializer
     permission_classes = [AllowAny]
+
+    
+    # === USERS ===
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email']
+
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
